a6755fcb17a7a00f1ab056940d97260f26c9a845
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-09-03 00:14:44 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,74 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendValue("spill", 2)
-    radio.sendValue("spill", 0)
-    radio.sendValue("spill", 1)
-    radio.sendValue("spill", 3)
-    radio.sendString("")
+function RiktigLøsning() {
+    bitbot.setLedColor(0x00FF00)
+    basic.pause(4000)
+    Restart()
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 10) {
+        LøstOppgave = true
+        RiktigLøsning()
+    }
 })
-input.onGesture(Gesture.Shake, function () {
-	
+function Restart() {
+    LøstOppgave = false
+    Sekvens = false
+    bitbot.setLedColor(0xFFFF00)
+}
+function FeilLøsning() {
+    bitbot.setLedColor(0xFF0000)
+    basic.pause(4000)
+    Restart()
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "P") {
+        Pitch = value * -20
+    } else if (name == "A") {
+        AvPå = value
+    } else if (name == "R") {
+        Roll = value * -20
+    }
+    if (!(Sekvens) && AvPå == 1) {
+        Sekvens = true
+        Kjøretid = input.runningTime()
+    }
 })
-radio.setGroup(4)
+let DriveRight = 0
+let DriveLeft = 0
+let Kjøretid = 0
+let Roll = 0
+let AvPå = 0
+let Pitch = 0
+let Sekvens = false
+let LøstOppgave = false
+bitbot.select_model(BBModel.XL)
+radio.setGroup(1)
+let Sekvenstid = 45000
+Restart()
+basic.forever(function () {
+    if (AvPå == 1) {
+        DriveLeft = Pitch - pins.map(
+            Roll,
+            0,
+            1023,
+            Pitch,
+            Pitch * -1
+        )
+        DriveRight = Pitch - pins.map(
+            Roll,
+            0,
+            -1023,
+            Pitch,
+            Pitch * -1
+        )
+        bitbot.motor(BBMotor.Left, Pitch - DriveLeft)
+        bitbot.motor(BBMotor.Right, Pitch - DriveRight)
+    } else {
+        bitbot.motor(BBMotor.Both, 0)
+    }
+    if (input.runningTime() - Kjøretid > Sekvenstid && !(LøstOppgave) && Sekvens) {
+        radio.sendNumber(11)
+        AvPå = 0
+        FeilLøsning()
+    }
+})
+
