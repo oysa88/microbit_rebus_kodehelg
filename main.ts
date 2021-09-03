a068bb03cb80d41a2a0d54da6eb0f07fe9ee8944
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-09-03 21:40:43 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendValue("spill", 2)
-    radio.sendValue("spill", 0)
-    radio.sendValue("spill", 1)
-    radio.sendValue("spill", 3)
-    radio.sendString("")
-})
-input.onGesture(Gesture.Shake, function () {
-	
-})
-radio.setGroup(4)
+
